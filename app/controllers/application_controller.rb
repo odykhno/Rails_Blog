6d3849c6892f2c25c7ceb7f4379eba002ba4666f
Olena Dykhno <olenadykhno@gmail.com>
2016-08-27 11:51:26 +0300

    add verification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def if_no_admin
+    render(file: 'public/422.html', status: :unauthorized) unless current_user.admin
+  end
+
+  def if_blocked_user
+    render(file: 'public/422.html', status: :blocked) if current_user.blocked
+  end
 end
