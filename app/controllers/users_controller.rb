ab456c9e0ba6a676e0745c1dcc46e078e38e9a20
Olena Dykhno <olenadykhno@gmail.com>
2016-08-27 11:08:08 +0300

    add column blocked to users, add oppotunity to block users for admins, fix render page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,26 @@ class UsersController < ApplicationController
   end
 
   def save
+    @users = User.all
     # binding.pry
     if params[:grant_admin]
       user = User.find_by(id: params[:grant_admin].to_f)
       user.admin = true
       user.save
-    else
+    elsif params[:grant_admin_hidden]
       user = User.find_by(id: params[:grant_admin_hidden].to_f)
       user.admin = false
       user.save
     end
-    render 'show' # doesn't work
+    if params[:block_user]
+      user = User.find_by(id: params[:block_user].to_f)
+      user.blocked = true
+      user.save
+    elsif params[:block_user_hidden]
+      user = User.find_by(id: params[:block_user_hidden].to_f)
+      user.blocked = false
+      user.save
+    end
+    render 'show'
   end
 end
