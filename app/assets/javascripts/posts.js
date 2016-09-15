ab1c52e5bafe68318316ffda3ed73c2cce039b91
Olena Dykhno <olenadykhno@gmail.com>
2016-09-15 15:27:57 +0300

    fix problem with link during ajax request

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,6 +1,11 @@
 $(function() {
     $('#new_comment').on('ajax:success', function(xhr, data, status) {
-        $('.table tbody').prepend('<tr><td>' + data.author + '</td><td>' + data.text + '</td></tr>');
+        console.log(data);
+        $('.table tbody').prepend('<tr><td>' + data.author + '</td><td>' + data.text + '</td><td>'
+        + '<span style="color:red">To have possibility delete this comment you need to reload this page!</span></td></tr>');
         $('#comment_text').val('');
     });
 });
+
+// '<a href="http://google.ru">ссылка</a>'
+// "<%= link_to 'Destroy Comment', [comment.post, comment], method: :delete, data: { confirm: 'Are you sure?' } %>"
