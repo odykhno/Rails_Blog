2929593503f96c880eea0dfd372846b78b1a9401
Olena Dykhno <olenadykhno@gmail.com>
2016-09-16 09:50:52 +0300

    fix bug with tags for deploying to heroku

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,16 @@ class Tag < ApplicationRecord
   has_many :taggings
   has_many :posts, through: :taggings
 
+  # def self.counts
+  #   self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
+  # end
+
   def self.counts
-    self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
+    Tag.select("tags.id, tags.name,count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
   end
 end
+
+# def self.tag_counts
+#   Tag.select("tags.id, tags.name,count(taggings.tag_id) as count").
+#       joins(:taggings).group("taggings.tag_id, tags.id, tags.name")
+# end
