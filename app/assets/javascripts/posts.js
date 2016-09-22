e201500c08692df86a2f3c5307a164aa38ad02ae
Olena Dykhno <olenadykhno@gmail.com>
2016-09-22 16:50:45 +0300

    finish chat realisation

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,25 +1,22 @@
-$(function() {
-    // $('#new_comment').on('ajax:success', function(xhr, data, status) {
-    //     // $('#comments_wrapper').html(data);
-    //     $('.table tbody').prepend('<tr><td>' + data.author + '</td><td>' + data.text + '</td><td>'
-    //     + '<span style="color:red">To have possibility delete this comment you need to reload this page!</span></td></tr>');
-    //     $('#comment_text').val('');
-    // });
-});
+// $(function() {
+//     $('#new_comment').on('ajax:success', function(xhr, data, status) {
+//         // $('#comments_wrapper').html(data);
+//         $('.table tbody').prepend('<tr><td>' + data.author + '</td><td>' + data.text + '</td><td>'
+//         + '<span style="color:red">To have possibility delete this comment you need to reload this page!</span></td></tr>');
+//         $('#comment_text').val('');
+//     });
+// });
 
 Pusher.logToConsole = true;
 
-var pusher = new Pusher('964185b17dbe78577e37', {
+var pusher = new Pusher('4f00e2203bc49efd9268', {
     cluster: 'eu',
     encrypted: true
 });
 
 var channel = pusher.subscribe('comments');
 channel.bind('new', function(data) {
-    // console.log(data);
-    $('.table tbody').prepend('<tr><td>' + data.author + '</td><td>' + data.text + '</td><td>'
-        + '<span style="color:red">To have possibility delete this comment you need to reload this page!</span></td></tr>');
-    // $('#' + data.commentable_type.toLowerCase() + '_comments_' + data.commentable_id + ' tbody')
-    //     .prepend('<tr><td>' + data.user_name + '</td><td>' + data.text + '</td></tr>');
+    $('#post_comments_' + data.post_id + ' tbody').prepend('<tr><td>' + data.author + '</td><td>' + data.text + '</td><td>'
+      + '<span style="color:red">To have possibility delete this comment you need to reload this page!</span></td></tr>');
     $('#comment_text').val('');
 });
