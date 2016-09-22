44fa348fb6ac0080bee7a267d0a22c556d72f820
Olena Dykhno <olenadykhno@gmail.com>
2016-09-22 09:29:49 +0300

    start to cerrect problem with ajax

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,8 +1,9 @@
 $(function() {
     $('#new_comment').on('ajax:success', function(xhr, data, status) {
         console.log(data);
-        $('.table tbody').prepend('<tr><td>' + data.author + '</td><td>' + data.text + '</td><td>'
-        + '<span style="color:red">To have possibility delete this comment you need to reload this page!</span></td></tr>');
+        $('#comments_wrapper').html(data);
+        // $('.table tbody').prepend('<tr><td>' + data.author + '</td><td>' + data.text + '</td><td>'
+        // + '<span style="color:red">To have possibility delete this comment you need to reload this page!</span></td></tr>');
         $('#comment_text').val('');
     });
 });
