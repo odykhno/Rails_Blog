0d0a26614e8447179089184121f45199fc08bc28
Olena Dykhno <olenadykhno@gmail.com>
2016-09-30 12:40:35 +0300

    start correct login through VK

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,13 @@
 class SessionsController < ApplicationController
   before_action :set_user
 
+  @@counter = 0
+
   def create
     @user.update(picture: auth_hash['info']['image'],
                  name: auth_hash['info']['name'],
-                 email: auth_hash['info']['email'] || "wrong@mail.ru")
+                 email: auth_hash['info']['email'] || 'wrong' + @@counter.to_s + '@mail.ru')
+    @@counter += 1
     sign_in @user
     redirect_to persons_profile_path
   end
