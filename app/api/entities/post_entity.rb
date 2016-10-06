8ed041f4e2e1711a8423aed887c716e8a6cf3586
Olena Dykhno <olenadykhno@gmail.com>
2016-10-06 16:16:00 +0300

    add timestamps toposts

--- app/api/entities/post_entity.rb
+++ app/api/entities/post_entity.rb
@@ -1,3 +1,10 @@
 class PostEntity < Grape::Entity
+  format_with(:iso_timestamp) { |dt| dt.iso8601 }
+
   expose :id, :title, :text
+
+  with_options(format_with: :iso_timestamp) do
+    expose :created_at
+    expose :updated_at
+  end
 end
